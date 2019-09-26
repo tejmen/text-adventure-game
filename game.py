0d90c58b38eebb5f588dc38029c186fca8a82191
tejmen <tej_men@outlook.com>
2019-09-26 17:33:05 +0100

    started off the code for this

---
+++ game.py
@@ -0,0 +1,15 @@
+# Python Text RPG
+# Created by tej_men :D
+
+import cmd
+import textwrap
+import sys
+import os
+import time
+import random
+
+screen_width = 100
+
+##### Player Setup #####
+class Player:
+    pass
