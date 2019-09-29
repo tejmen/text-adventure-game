f775724f36a4d54b680840d1f4cbb11ce6e778d3
tejmen <tej_men@outlook.com>
2019-09-29 16:23:39 +0100

    added title screen

--- game.py
+++ game.py
@@ -11,10 +11,33 @@ import random
 screen_width = 100
 
 ##### Player Setup #####
-class Player:
+class player:
     def __init__(self):
         self.name = ''
         self.hp = 0
         self.mp = 0
         self.status_effects = []
-myPlayer = Player()
+myPlayer = player()
+
+##### Title Screen #####
+def title_screen_selections():
+    option = input("> ")
+    if option.lower().strip() == ("play"):
+        start_game() # placeholder until written
+    elif option.lower().strip() == ("help"):
+        help_menu()
+    elif option.lower().strip() == ("quit"):
+        sys.exit()
+    while option.lower().strip() not in ['play', 'help', 'quit']:
+        print("Please enter a valid command.")
+        option = input("> ")
+    if option.lower().strip() == ("play"):
+        start_game() # placeholder until written
+    elif option.lower().strip() == ("help"):
+        help_menu()
+    elif option.lower().strip() == ("quit"):
+        sys.exit()
+
+def title_screen():
+    os.system('clear')
+    print('############################')
