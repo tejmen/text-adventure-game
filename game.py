1cb6426f73d9a18dd72ba9f5756c922c1bab0ce4
Meena Mengle <Meena.Mengle@westherts.ac.uk>
2020-05-30 12:07:26 +0100

    added turn based combat

--- game.py
+++ game.py
@@ -1,5 +1,8 @@
 # Python Text RPG
-# Created by tej_men :D)
+# Created by tej_men
+#     \
+# (:D)-<
+#     /
 
 
 import cmd
@@ -10,18 +13,28 @@ import time
 import random
 
 screen_width = 100
-godbool=False
+godbool = '1'
 ##### Player Setup #####
 class player:
     def __init__(self):
         self.name = ''
         self.job = ''
         self.hp = 0
-        self.mp = 0
+        self.ap = 0
         self.status_effects = []
         self.location = 'b2'
         self.game_over = False
+        self.inventory = ''
+        self.weapon = ''
 myPlayer = player()
+
+class Gslime:
+    def __init__(self):
+        self.hp = 300
+        self.ap = 20
+GreenS = Gslime()
+while GreenS.hp <= 0:
+    GreenS.hp = 0
 
 ##### Title Screen #####
 def title_screen_selections():
@@ -34,6 +47,8 @@ def title_screen_selections():
         sys.exit()
     elif option.lower().strip() == ('resume'):
         main_game_loop()
+    elif option.lower().strip() == ("acknowledgements"):
+        acknowledgements_menu()
     elif option.lower().strip() == '/god':
         print('God mode not enabled, you cheat.')
         print('PLAY THE GAME PROPERLY YOU WEASAL!')
@@ -42,12 +57,12 @@ def title_screen_selections():
 
         if godanswer == 'a':
             print('Hello god. Fancy seeing you here.')
-            godbool = True
+            godbool = 'yes'
             title_screen()
 
         else:
             title_screen()
-    while option.lower().strip() not in ['play', 'help', 'quit', 'resume', '/god']:
+    while option.lower().strip() not in ['play', 'help', 'quit', 'resume', '/god', 'acknowledgements']:
         print("Please enter a valid command.")
         option = input("> ")
         if option.lower().strip() == ("play"):
@@ -58,6 +73,8 @@ def title_screen_selections():
             sys.exit()
         elif option.lower().strip() == ('resume'):
             main_game_loop()
+        elif option.lower().strip() == ("acknowledgements"):
+            acknowledgements_menu()
         elif option.lower().strip() == '/god':
             print('God mode not enabled, you cheat.')
             print('PLAY THE GAME PROPERLY YOU WEASAL!')
@@ -65,7 +82,7 @@ def title_screen_selections():
             godanswer = input()
             if godanswer == 'a':
                     print('Hello god. Fancy seeing you here.')
-                    godbool = True
+                    godbool = 'yes'
                     title_screen()
 
 def title_screen():
@@ -74,25 +91,42 @@ def title_screen():
     print('#         - Play -         #')
     print('#        - Resume -        #')
     print('#         - Help -         #')
+    print('#   - Acknowledgements -   #')
     print('#         - Quit -         #')
     print('# Copyright 2019 tejmen09  #')
     title_screen_selections()
 
 def help_menu():
-    print('############################')
-    print('#           Help           #')
-    print('############################')
-    print('- Use move command to move  ')
-    print('- Type your commands to do  ')
-    print('  them                      ')
-    print('- Use "look" to inspect     ')
-    print('  something                 ')
-    print('  Copyright 2019 tejmen09   ')
+    print('###############################')
+    print('#            Help             #')
+    print('# • Use move command to move  #')
+    print('# • Type your commands to do  #')
+    print('#   them                      #')
+    print('# • Use "look" to inspect     #')
+    print('#   something                 #')
+    print('#   Copyright 2019 tejmen09   #')
+    print('###############################')
+    time.sleep(5)
+    title_screen()
+def acknowledgements_menu():
+    print('###############################')
+    print('#       Acknowledgements      #')
+    print('# Beta Tested By:             #')
+    print('# • Manas Mengle              #')
+    print('# • Pranot Mengle             #')
+    print('# • Mennakshi Mengle          #')
+    print('# Helped By:                  #')
+    print('# • Manas Mengle              #')
+    print('# • Pranot Mengle             #')
+    print('# Copyright 2019 Tejas Mengle #')
+    print('###############################')
     time.sleep(5)
     title_screen()
 
+
+
 ##### MAP #####
-#     1   2   3   4     PLAYER STARTS AT b2
+#     1     2    3    4     PLAYER STARTS AT b2
 #   ---------------------
 #   |   |       |   |   |  a
 #   ---------------------
@@ -163,7 +197,7 @@ zonemap = {
     'b1': {
         ZONENAME : 'Chest',
         DESCRIPTION : 'This is an unlocked chest!',
-        EXAMINATION : 'You found a sharp sword!',
+        EXAMINATION : 'The key is already in the Chest lock.',
         SOLVED : False,
         UP : 'a1',
         DOWN : 'c1',
@@ -184,10 +218,10 @@ zonemap = {
         ZONENAME : 'Forest',
         DESCRIPTION : 'This is an oak forest.',
         EXAMINATION : 'This forest has a crossway so you can go any way you want',
-        SOLVED : False,
+        SOLVED : True,
         UP : 'a3',
         DOWN : 'c3',
-        LEFT : 'up',
+        LEFT : 'b2',
         RIGHT : 'b4',
     },
     'b4': {
@@ -198,7 +232,7 @@ zonemap = {
         UP : 'a4',
         DOWN : 'c4',
         LEFT : 'b3',
-        RIGHT : '',
+        RIGHT : 'b4',
     },
     'c1': {
         ZONENAME : '???',
@@ -214,7 +248,7 @@ zonemap = {
         ZONENAME : 'Forest',
         DESCRIPTION : 'This is an ash forest.',
         EXAMINATION : 'This forest has a crossway so you can go any way you want',
-        SOLVED : False,
+        SOLVED : True,
         UP : 'b2',
         DOWN : 'd2',
         LEFT : 'c1',
@@ -224,7 +258,7 @@ zonemap = {
         ZONENAME : 'Forest',
         DESCRIPTION : 'This is a birch forest.',
         EXAMINATION : 'This forest has a crossway so you can go any way you want.',
-        SOLVED : False,
+        SOLVED : True,
         UP : 'b3',
         DOWN : 'd3',
         LEFT : 'c2',
@@ -273,7 +307,7 @@ zonemap = {
     'd4': {
         ZONENAME : 'End Portal',
         DESCRIPTION : 'This is where you go if you want to stop exploring.',
-        EXAMINATION : 'examine',
+        EXAMINATION : 'This is a way to go to the main menu.',
         SOLVED : False,
         UP : 'c4',
         DOWN : 'd4',
@@ -281,7 +315,6 @@ zonemap = {
         RIGHT : 'd4',
     },
 }
-
 
 ##### GAME INTERACTIVITY #####
 def print_location():
@@ -293,9 +326,9 @@ def print_location():
 def prompt():
     print('\n' + '===============================')
     print('What would you like to do?')
-    print("(You can 'move', 'quit' or 'look')")
+    print("(You can 'move', 'quit', 'look' or 'act')")
     action = input('> ')
-    acceptable_actions = ['move', 'quit', 'look', '/god']
+    acceptable_actions = ['move', 'quit', 'look', '/god', 'acceptable_actions', 'act']
     while action.lower() not in acceptable_actions:
         print('Unknown action, try again.\n')
         action = input('> ')
@@ -309,6 +342,12 @@ def prompt():
         print('God mode not enabled, you cheat.')
         print('PLAY THE GAME PROPERLY YOU WEASAL!')
         print("DON'T CHEAT EVER AGAIN!")
+    elif action.lower().strip() == 'acceptable_actions':
+        print('You have found an easter egg.')
+        print("Don't tell anyone." + " ;)")
+    elif action.lower().strip() == 'act':
+        player_act()
+
 def player_move():
 
     ask = 'Where would you like to move to?\n'
@@ -330,8 +369,13 @@ def player_move():
     elif dest == 'right' or dest == 'east':
         destination = zonemap[myPlayer.location][RIGHT]
         movement_handler(destination)
-    elif dest == 'tp': #and godbool == True:
-        teleport()
+    #if godbool == 'yes':
+    if dest == 'tp':
+        print('Where do you want to go god?')
+        qa = input('_')
+        myPlayer.location = qa
+        destination = myPlayer.location
+        movement_handler(destination)
 
 def movement_handler(destination):
     print('\n'+ 'You have moved to ' + destination + '.')
@@ -380,6 +424,7 @@ def setup_game():
         myPlayer.job = player_job
         print('you are now a ' + player_job + '!\n')
     while player_job.lower() not in valid_jobs:
+        print('Unknown action, try again.\n')
         player_job = input("> ")
         if player_job.lower() in valid_jobs:
             myPlayer.job = player_job
@@ -387,13 +432,16 @@ def setup_game():
     ### PLAYER STATS ###
     if myPlayer.job == 'fighter':
         myPlayer.hp = 120
-        myPlayer.mp = 20
+        myPlayer.ap = 50
+        myPlayer.weapon = 'sword'
     elif myPlayer.job == 'wizard':
-        myPlayer.hp = 40
-        myPlayer.mp = 120
+        myPlayer.hp = 300
+        myPlayer.ap = 30
+        myPlayer.weapon = 'staff'
     elif myPlayer.job == 'healer':
-        myPlayer.hp = 60
-        myPlayer.mp = 60
+        myPlayer.hp = 200
+        myPlayer.ap = 40
+        myPlayer.weapon = 'magic book'
 
     ### INTRODUCTION ###
     question3 = 'Welcome ' + player_name +' the ' + player_job + '.\n'
@@ -424,17 +472,128 @@ def setup_game():
         sys.stdout.flush()
         time.sleep(0.02)
 
-
     print('############################')
     print('#       Lets Jump In!      #')
     print('############################')
     main_game_loop()
-def teleport():
-    print('Where do you want to go god?')
-    qa = input('_')
-    myPlayer.location = qa
-    destination = myPlayer.location
-    movement_handler(destination)
 
+def player_act():
+    if zonemap[myPlayer.location][SOLVED] == False:
+        if myPlayer.location == 'b4':
+            myPlayer.location = 'c1'
+            destination = myPlayer.location
+            movement_handler(destination)
+        elif myPlayer.location == 'c1':
+            myPlayer.location = 'b4'
+            destination = myPlayer.location
+            movement_handler(destination)
+        elif myPlayer.location == 'd4':
+            title_screen()
+        elif myPlayer.location == 'a1':
+            zonemap[myPlayer.location][SOLVED] = True
+            print('I was going to buy an apple, but I dont have any money.')
+        elif myPlayer.location == 'a2':
+            zonemap[myPlayer.location][SOLVED] = True
+            print('There is nothing you can do ' + myPlayer.name + ' the ' + myPlayer.job)
+        elif myPlayer.location == 'a3':
+            zonemap[myPlayer.location][SOLVED] = True
+            print('The mayoral election are going on. I wish I could vote, but I am too young.')
+        elif myPlayer.location == 'a4':
+            zonemap[myPlayer.location][SOLVED] = True
+            print("There is a weird smell of rotten eggs inside. I'm not going inside!")
+        elif myPlayer.location == 'b1':
+            zonemap[myPlayer.location][SOLVED] = True
+            print('You pick up the ' + myPlayer.weapon)
+            myPlayer.inventory = myPlayer.weapon
+        elif myPlayer.location == 'b2':
+            zonemap[myPlayer.location][SOLVED] = True
+            print1 = 'You take a nap'
+            for character in print1:
+                sys.stdout.write(character)
+                sys.stdout.flush()
+                time.sleep(0.1)
+            time.sleep(1)
+        elif myPlayer.location == 'b3':
+            zonemap[myPlayer.location][SOLVED] = True
+            print('There is nothing you can do ' + myPlayer.name + ' the ' + myPlayer.job)
+        elif myPlayer.location == 'c2':
+            zonemap[myPlayer.location][SOLVED] = True
+            print('There is nothing you can do ' + myPlayer.name + ' the ' + myPlayer.job)
+        elif myPlayer.location == 'c3':
+            zonemap[myPlayer.location][SOLVED] = True
+            print('There is nothing you can do ' + myPlayer.name + ' the ' + myPlayer.job)
+        elif myPlayer.location == 'c4':
+            turn_based_combat()
+        elif myPlayer.location == 'd1':
+            zonemap[myPlayer.location][SOLVED] = True
+            print('This beach is too rocky and full of seaweed to do anything ' + myPlayer.name + ' the ' + myPlayer.job)
+        elif myPlayer.location == 'd2':
+            zonemap[myPlayer.location][SOLVED] = True
+            print('this beach is so sandy that...')
+            print1 = 'you take a nap.'
+            for character in print1:
+                sys.stdout.write(character)
+                sys.stdout.flush()
+                time.sleep(0.1)
+            time.sleep(1)
+        elif myPlayer.location == 'd3':
+            turn_based_combat()
+    else:
+        print('This place has already been excavated...')
+        print('BY YOU!')
+def turn_based_combat():
+    print('##########################################')
+    print('# A Green Slime appeared out of the dark #')
+    print('##########################################')
+    while GreenS.hp > 0:
+        print('What would you like to do?')
+        print("(You can type 'attack' to attack the Green slime.)")
+        attack = input('> ')
+        acceptable_attack = ['attack','kill']
+        while attack.lower().strip() not in acceptable_attack:
+            print('Unknown action, try again.\n')
+            attack = input('> ')
+            if attack.lower().strip() == 'attack':
+                GreenS.hp = GreenS.hp - myPlayer.ap
+                print("The green slime's health is ")
+                print(GreenS.hp)
+            elif attack.lower().strip() == 'kill':
+                GreenS.hp = 0
+                print("The green slime's health is ")
+                print(GreenS.hp)
+        if attack.lower().strip() == 'attack':
+            GreenS.hp = GreenS.hp - myPlayer.ap
+            print("The green slime's health is ")
+            print(GreenS.hp)
+        elif attack.lower().strip() == 'kill':
+            GreenS.hp = 0
+            print("The green slime's health is ")
+            print(GreenS.hp)
+        if attack.lower().strip() == 'heal':
+            if myPlayer.job == 'healer':
+                print('poop')
+        if GreenS.hp > 0:
+            print('################################')
+            print('# The Green Slime attacked you.#')
+            print('################################')
+            myPlayer.hp = myPlayer.hp - GreenS.ap
+            text = 'You have '
+            text2 = 'health left.'
+            for character in text:
+                sys.stdout.write(character)
+                sys.stdout.flush()
+                time.sleep(0.1)
+            print(myPlayer.hp)
+            for character in text2:
+                sys.stdout.write(character)
+                sys.stdout.flush()
+                time.sleep(0.1)
+    print('####################################')
+    print('# You have defeated a green slime. #')
+    print('#     You have gained 100 XP.      #')
+    print('####################################')
+    GreenS.hp = 300
+    zonemap[myPlayer.location][SOLVED] = True
+    main_game_loop()
 
 title_screen()
